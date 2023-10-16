2f038cb31e13b2329c2ccf926aabf14d2c17f367
Urvi2119 <63640485+Urvi2119@users.noreply.github.com>
2023-10-16 14:26:00 +0530

    Update Final.py

--- Final.py
+++ Final.py
@@ -2,8 +2,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor  # Import Random Forest Regressor
+# Import Random Forest Regressor
+from sklearn.ensemble import RandomForestRegressor
 import random
+import re
+
+# def is_valid_format(value):
+#     return re.match(r'^\s*\$\s*\d+\.\d{2}\s*$', str(value)) is not None
+
+
+def is_valid_format(value):
+    return re.match(r'^\s*\$?\d+(\.\d{2})?\s*$', str(value)) is not None
+
+
+def clean_brand_name(brand):
+    # Example data cleaning steps:
+    # 1. Convert to lowercase
+    # 2. Remove leading and trailing spaces
+    # 3. Handle special characters or variations
+    brand = brand.lower().strip()
+
+    # Additional cleaning steps if needed
+    # Example: Replace '&' with 'and'
+    brand = brand.replace('&', 'and')
+
+    return brand
+    # Remove spaces and special characters, replace with underscores
+    # cleaned_name = re.sub(r'[^a-zA-Z0-9]', '_', brand_name)
+    # return cleaned_name
+
 
 # Global variables
 forest_bud = 0
@@ -18,24 +45,49 @@ try:
         uploaded_file_df_orig = uploaded_file_df
     uploaded_file_df['Year'] = uploaded_file_df['Year'].astype(str)
     uploaded_file_df['Year'] = uploaded_file_df['Year'].str.replace(",", "")
+    uploaded_file_df['A&P'] = uploaded_file_df['A&P'].astype(str)
+    uploaded_file_df['A&P'] = uploaded_file_df['A&P'].str.replace("$", "")
     st.write(uploaded_file_df)
-    columns = ['Company', 'Brand', 'Product',
-               'Actual', 'Month', 'Year', 'Budgeted']
-    uploaded_file_df_1 = uploaded_file_df[columns]
 
-    # Converting Categorical data into Numerical data
-    uploaded_file_df_1 = pd.get_dummies(uploaded_file_df_1, columns=[
-        'Company', 'Brand', 'Month', 'Product'])
-    st.subheader("Clean Data:")
+    columns_to_add = ['Price (US/ TT)', 'A&P']
+
+    # Remove leading/trailing spaces from column names
+    uploaded_file_df.columns = uploaded_file_df.columns.str.strip()
+
+    uploaded_file_df_1 = uploaded_file_df  # Create a copy of the DataFrame
+
+    for column in columns_to_add:
+        if column not in uploaded_file_df_1.columns:
+            uploaded_file_df_1[column] = 0  # Add the column with None values
+
+    uploaded_file_df_1 = uploaded_file_df_1.drop(columns=['Dummy'])
 
     # Filling the missing values with mean
     uploaded_file_df_1["Actual"] = uploaded_file_df_1["Actual"].fillna(
         uploaded_file_df_1["Actual"].mean())
     uploaded_file_df_1.isna().sum()
-    st.write(uploaded_file_df_1)
+    uploaded_file_df_1["A&P"] = uploaded_file_df_1["A&P"].fillna(0.0)
+    # Clean the "A&P" column using regular expressions and replace problematic values with 0
+    uploaded_file_df_1["A&P"] = uploaded_file_df_1["A&P"].apply(
+        lambda x: '0.0' if not is_valid_format(x) else x)
 
-    # Converting the Year column into integer
-    uploaded_file_df['Year'] = uploaded_file_df['Year'].astype(int)
+    uploaded_file_df_1["A&P"] = uploaded_file_df_1['A&P'].astype(str)
+
+    # Clean the 'A&P' column
+    uploaded_file_df_1["A&P"] = uploaded_file_df_1["A&P"].apply(
+        lambda x: '0.0' if not re.match(r'^\d+(\.\d+)?$', str(x)) else x)
+
+    # Convert 'A&P' column to float type
+    uploaded_file_df_1["A&P"] = uploaded_file_df_1["A&P"].str.replace(
+        '[^\d.]', '', regex=True).astype(float)
+    # Remove '$' from "Price (US/ TT)" column
+    uploaded_file_df_1["Price (US/ TT)"] = uploaded_file_df_1["Price (US/ TT)"].str.replace(
+        '[\$,]+', '', regex=True)
+    uploaded_file_df_1["Price (US/ TT)"] = uploaded_file_df_1["Price (US/ TT)"].astype(float)
+    uploaded_file_df_1 = pd.get_dummies(uploaded_file_df_1, columns=[
+        'Company', 'Brand', 'Month', 'Product', 'Price (US/ TT)', 'A&P'])
+    st.subheader("Clean Data:")
+    st.write(uploaded_file_df_1)
 
     # Splitting the data into training and testing modules
     X = uploaded_file_df_1.drop(columns=['Actual', 'Budgeted'])
@@ -56,6 +108,9 @@ try:
     # Fit the model
     forest_reg.fit(train_X, train_y)
 
+    # Fit the model
+    # forest_reg.fit(X, y)  # Train the model with all possible feature values
+
     # Testing and training accuracy
     forest_train_score = forest_reg.score(train_X, train_y)
     forest_test_score = forest_reg.score(test_X, test_y)
@@ -72,12 +127,14 @@ try:
     for index, row in uploaded_file_df.iterrows():
         lst_temp = []
         # Prepare the input data for the model
-        Company_name = row['Company']
-        Brand_name = row['Brand']
-        Product_value = row['Product']
-        Month_name = row['Month']
-        Year_value = row['Year']
-        Actual = row['Actual']
+        Company_name = str(row['Company'])
+        Brand_name = str(row['Brand'])
+        Product_value = str(row['Product'])
+        Month_name = str(row['Month'])
+        Year_value = str(row['Year'])
+        Actual = str(row['Actual'])
+        Price = str(row['Price (US/ TT)'])
+        AP = str(row['A&P'])
 
         Company = 'Company_' + Company_name
         Brand = 'Brand_' + Brand_name
@@ -85,11 +142,14 @@ try:
         Month = 'Month_' + Month_name
 
         # Create a new dataframe with the input data
-        new_data = pd.DataFrame({f'{Company}': [1],
-                                 f'{Brand}': [1],
-                                 f'{Product}': [1],
-                                 f'{Month}': [1],
-                                 'Year': [int(Year_value)]})
+        new_data = pd.DataFrame({f'{Company}': 1,
+                                 f'{Brand}': 1,
+                                 f'{Product}': 1,
+                                 f'{Month}': 1,
+                                 'Year': int(Year_value),
+                                 f'Price (US/ TT)': 1,
+                                 f'A&P': 1,
+                                 }, index=[0])
 
         # Preprocess the new data
         new_data = pd.get_dummies(new_data, drop_first=True)
@@ -101,9 +161,11 @@ try:
         forest_prediction = forest_reg.predict(new_data)[0]
 
         # Add the predicted value to the list
-        lst_temp.append(forest_prediction)
-        lst_temp.append(row['Actual'])
-        lst_temp.append(row['Budgeted'])
+        lst_temp.append(str(forest_prediction))
+        lst_temp.append(str(row['Actual']))
+        lst_temp.append(str(row['Budgeted']))
+        lst_temp.append(str(row['Price (US/ TT)']))
+        lst_temp.append(str(row['A&P']))
         lst.append(lst_temp)
 
         result_temp.append({
@@ -114,19 +176,25 @@ try:
             'Year': str(Year_value).replace(',', ''),
             'Prediction': forest_prediction,
             'Actual': Actual,
+            'Price (US/ TT)':  Price,
+            'A&P': AP,
+
         })
 
     company_list = uploaded_file_df['Company'].unique()
     product_list = uploaded_file_df['Product'].unique()
     month_list = uploaded_file_df['Month'].unique()
-    month_list = np.delete(month_list, 12)
+    month_list = np.delete(month_list, 11)
     actual_list = uploaded_file_df['Actual'].unique()
     brand_list = uploaded_file_df['Brand'].unique()
+    unique_prices = uploaded_file_df['Price (US/ TT)'].unique()
+    unique_ap_values = uploaded_file_df['A&P'].unique()
+
     company_and_brand_Dict = {}
     brand_product_dict = {}
     for company in company_list:
-        related_brands = uploaded_file_df.loc[uploaded_file_df['Company']
-                                            == company, 'Brand'].unique()
+        related_brands = uploaded_file_df.loc[uploaded_file_df['Company'] == company, 'Brand'].unique(
+        )
         company_and_brand_Dict[company] = related_brands
         brand_product_dict[company] = {}
         for brand in related_brands:
@@ -161,54 +229,103 @@ try:
     st.write(f"Random {random_value} values from the dataset")
     st.line_chart(data=traildf)
 
-    # Created selection fields for the user to select the Company, Brand, Product, Month and Year
+    # Create multi-select fields for selecting multiple Companies, Brands, and Products
     st.subheader("Prediction for new periods :")
-    st.subheader(
-        'Please select a Company and a Year for Brands, Products, and Months :')
+    st.subheader('Please select Companies, Brands, Products, and Year:')
+
     selected_company = st.selectbox('Company', company_list)
-    to_show_brand = company_and_brand_Dict[selected_company]
+    to_show_brand = []
+
+    for brand in company_and_brand_Dict[selected_company]:
+        to_show_brand.append(brand)
+
     selected_brand = st.multiselect("Brands", to_show_brand)
+    selected_brand = set(selected_brand)
     len_list = []
-    for i in range(len(selected_brand)):
-        len_list.append(
-            list(brand_product_dict[selected_company][selected_brand[i]]))
 
-    max_index = max(range(len(len_list)), key=lambda i: len(len_list[i]))
-    to_show_product = brand_product_dict[selected_company][selected_brand[max_index]]
-    selected_product = st.multiselect("Products", to_show_product)
+    brand_product_dicti = {}
+    to_show_products = []
+
+    related_brands = company_and_brand_Dict[selected_company]
+    for brand in related_brands:
+        if brand in tuple(selected_brand):
+            for prod in brand_product_dict[selected_company][brand]:
+                if prod not in to_show_products:
+                    to_show_products.append(prod)
+
+    selected_product = st.multiselect("Products", to_show_products)
     selected_month = st.multiselect("Months", month_list)
+    if len(selected_month) > 3:
+        st.warning("Please select only three months.")
+        selected_month = selected_month[:3]
     input_year = st.text_input('Year')
+
+    # selected_price = [st.selectbox(
+    #     'Price (US/ TT)', unique_prices,  label_visibility="hidden")]
+    # selected_ap_values = [st.selectbox(
+    #     'A&P', unique_ap_values,  label_visibility="hidden", index=1)]
+
+    # Collect the columns used for training the model
+    training_columns = train_X.columns
+
     results = []
+    selectedcompany = selected_company
+    selectedbrand = selected_brand
+    selectedproduct = selected_product
+    # Generate predictions for all selected combinations
+    for month in selected_month:
 
-    for brand in selected_brand:
-        for product in selected_product:
-            for month in selected_month:
+        for selected_brand in selectedbrand:
+            for selected_product in selectedproduct:
                 input_Company = 'Company_' + selected_company
-                input_Brand = 'Brand_' + brand
-                input_Product = 'Product_' + product
-                input_Month = 'Month_' + month
+                input_Brand = 'Brand_' + selected_brand
+                input_Product = 'Product_' + selected_product
+                input_Month = 'Month_' .join(month)
 
-                new_data = pd.DataFrame({f'{input_Company}': [1],
-                                         f'{input_Brand}': [1],
-                                         f'{input_Product}': [1],
-                                         f'{input_Month}': [1],
-                                         'Year': [int(input_year)]})
-                new_data = pd.get_dummies(new_data, drop_first=True)
+                new_data = pd.DataFrame({f'{input_Company}': 1,
+                                        f'{input_Brand}': 1,
+                                         f'{input_Product}': 1,
+                                         # f'{input_Month}': 1,
+                                         'Year': int(input_year)}, index=[0])
+
+                # Preprocess the new data
+                # new_data = pd.get_dummies(new_data, drop_first=True)
+
                 # Reindex the columns to match the training data
-                new_data = new_data.reindex(
-                    columns=train_X.columns, fill_value=0)
+                # new_data = new_data.reindex(
+                #     columns=train_X.columns, fill_value=0)
+
+                # # Add selected Price(US/TT) columns
+                # for price_option in selected_price:
+                #     if price_option != 'Empty':
+                #         price_option = str(price_option)
+                #         price_option = price_option.split('$')
+                #         price_option = str(price_option[1])
+                #         new_data[f'Price (US/ TT)_{price_option}'] = 1
+
+                # # Add selected A&P columns
+                # for ap in selected_ap_values:
+                #     if ap != 'Empty':
+                #         ap = str(ap)
+                #         new_data[f'A&P_{ap}'] = 1
 
                 # Predict the value using Random Forest
-                forest_prediction = forest_reg.predict(new_data)[0]
+                # forest_prediction = forest_reg.predict(new_data)[0]
 
                 # Append the results to the list
                 results.append({
                     'Company': input_Company.replace('Company_', ' '),
                     'Brand': input_Brand.replace('Brand_', ' '),
                     'Product': input_Product.replace('Product_', ' '),
-                    'Month': input_Month.replace('Month_', ' '),
-                    "Year": input_year,
-                    'Prediction': forest_prediction
+                    'Month': month.replace('Month_', ' '),
+
+                    "Year": str(input_year),
+
+                    # 'Price (US/ TT)': ', '.join(map(str, selected_price)),
+                    # 'A&P': ', '.join(map(str, selected_ap_values)),
+                    'Price (US/ TT)': '$0.00',
+                    'A&P': 0.00,
+                    # 'Prediction': 0
                 })
 
     # Convert the results list to a dataframe
@@ -218,21 +335,87 @@ try:
     df_results.to_csv('results.csv', index=False)
 
     st.subheader("Prediction for new period :")
-    # Print the dataframe in streamlit
-    st.write(df_results)
+
+    # Make the DataFrame editable
+    edited_df = st.experimental_data_editor(df_results)
 
     st.download_button(
         label="Download data as CSV",
-        data=df_results.to_csv().encode('utf-8'),
+        data=edited_df.to_csv().encode('utf-8'),
         file_name='large_df.csv',
         mime='text/csv',
     )
 
-    st.write(
-        "How many Times Random Forest Predicted is Better than Budgeted", forest_pre)
-    st.write("How many Times Budgeted is Better than Predicted", forest_bud)
+    # Read the uploaded CSV file into a DataFrame
+    # uploaded_csv = st.file_uploader("Upload your CSV file here", type="csv")
+    if st.button('Recalculate Prediction'):
+        # Extract the 'Price (US/ TT)' and 'A&P' columns from the edited_df
+        edited_prices = edited_df['Price (US/ TT)']
+        edited_ap = edited_df['A&P']
 
-    st.header("Thank you!")
+        # Update the 'Price (US/ TT)' and 'A&P' columns in uploaded_file_df_1 with the edited values
+        uploaded_file_df_1['Price (US/ TT)'] = edited_prices
+        uploaded_file_df_1['A&P'] = edited_ap
+
+        results2 = []
+
+        for index, (row_edited, row_uploaded) in enumerate(zip(edited_df.iterrows(), uploaded_file_df_1.iterrows())):
+            # Extract the 'Company' value from the edited DataFrame
+            # Use the 'Company' value from the edited DataFrame
+            Company_name = row_edited[1]['Company']
+
+            Brand_name = 'Brand_' + row_edited[1]['Brand']
+            Product_value = 'Product_' + row_edited[1]['Product']
+            Month_name = 'Month_' + row_edited[1]['Month']
+            Year_value = row_edited[1]['Year']
+            Actual = row_uploaded[1]['Actual']
+            Price = row_edited[1]['Price (US/ TT)']
+            A_P = row_edited[1]['A&P']
+
+            # Create a new DataFrame with the input data
+            new_data = pd.DataFrame({
+                f'Company_{Company_name}': 1,
+                f'{Brand_name}': 1,
+                f'{Product_value}': 1,
+                f'{Month_name}': 1,
+                'Year': int(Year_value),
+                'Price (US/ TT)': row_uploaded[1]['Price (US/ TT)'],
+                'A&P': row_uploaded[1]['A&P'],
+            }, index=[0])
+
+            # Preprocess the new data
+            new_data = pd.get_dummies(new_data, drop_first=True)
+
+            # Reindex the columns to match the training data
+            new_data = new_data.reindex(columns=train_X.columns, fill_value=0)
+
+            # Predict the value using Random Forest
+            forest_prediction = forest_reg.predict(new_data)[0]
+
+            # Append the results to the list
+            results2.append({
+                'Company': Company_name,  # Use the 'Company' value from the edited DataFrame
+                'Brand': Brand_name,
+                'Product': Product_value,
+                'Month': Month_name,
+                'Year': Year_value,
+                'Price (US/ TT)': Price,
+                'A&P': A_P,
+                'Prediction': forest_prediction
+            })
+
+        # Convert the results list to a DataFrame
+        df_results2 = pd.DataFrame(results2)
+
+        # Display the recalculated predictions
+        st.subheader("Recalculated Prediction:")
+        st.write(df_results2)
+        st.header("Thank you!")
+
+# except Exception as e:
+#     if str(e) != "name 'uploaded_file_df' is not defined":
+#         st.error(f"An error occurred: {str(e)}")
+
 except:
     pass
 
